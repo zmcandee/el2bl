9de540cbe119b2cdd7329423398b74ebf08e3779
Zach Candee <zcandee@xes-inc.com>
2023-08-31 16:49:14 -0500

    Complete rewrite removing BS dependency

--- el2bl.py
+++ el2bl.py
@@ -1,99 +1,78 @@
 #!/usr/bin/env python3
-"""el2bl: convert Evernote note links to Bear note links"""
 import os
 import re
-import argparse
-import warnings
-from bs4 import BeautifulSoup, CData, XMLParsedAsHTMLWarning
 
-warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
-
-BEAR_LINK_ESCAPE = r"([\\\[\]|/])"
+from argparse import ArgumentParser, ArgumentTypeError
 
 
-def input_enex_path():
-    """Read .enex files in directory.
-    ---
-    - Accept path to directory from user input
-    - Verify that directory is valid with os.path.exists()
-    - Scan directory with os.scandir and create files object
-    - Create directory for converted files
-    - Run function to convert links in each file
+def valid_dir(path: str) -> str:
+    if not os.path.isdir(path):
+        raise ArgumentTypeError(f"Path {path} is not a valid directory")
+    return path
+
+
+def parse_enex(file: str) -> str:
     """
-    parser = argparse.ArgumentParser(
-        description="Cleans Evernote enex files to be Bear compatible"
+    Open the file as read only and return a cleaned string
+    """
+    with open(file, "r") as enex:
+        soup = enex.read()
+
+        soup_sub = re.sub(
+            r'(<a[^>]*href="evernote[^>]*?>)(.*?)(</a>)',
+            lambda x: "[[{}]]".format(x.group(2).replace("/", "\\/")),
+            soup,
+        )
+
+        soup_sub = re.sub(
+            r'(<span style="color:rgb[^>]*?>)(.*?)(</span>)', r"==\2==", soup_sub
+        )
+
+    return soup_sub
+
+
+def main() -> None:
+    # Parse all the arguments
+    parser = ArgumentParser(
+        description="Clean Evernote enex files to be Bear compatible"
     )
+    parser.add_argument("enex_dir", type=valid_dir, help="Path to enex directory")
     parser.add_argument(
-        "path", type=str, nargs="?", help="file path to directory of evernote files"
-    )
-    parser.add_argument(
-        "-d",
-        dest="debug",
-        default=False,
-        const=True,
-        action="store_const",
-        help="Enable more debug output",
+        "-o",
+        "--output_dir",
+        type=valid_dir,
+        help="Path to the output directory for clean files",
     )
     args = parser.parse_args()
-    if args.path:
-        path = args.path
-    else:
-        path = input("Please input the path to a directory with Evernote exports: ")
-    if not os.path.exists(path):
-        print(f"Not a valid file path:\n{path}")
-        exit
-    elif os.path.isfile(path):
-        print(f"Not a valid directory path: {path}")
-        exit
-    elif not os.listdir(path):
-        print(f"Directory is empty: {path}")
-        exit
-    else:
-        print(f"Valid directory path: {path}")
 
-    if not os.path.exists(f"{path}/bear"):
-        os.mkdir(f"{path}/bear")
-    for file in os.scandir(path):
-        if file.is_file() and file.name.endswith(".enex"):
-            try:
-                convert_links(file)
-            except Exception as e:
-                if args.debug:
-                    raise
-                print(f"An error occured:\n{e}\nPlease try again.")
+    if not args.output_dir:
+        args.output_dir = os.path.join(args.enex_dir, "bear")
 
+    if not os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
 
-def convert_links(file):
-    """Convert links in .enex files to Bear note link format.
-    ---
-    - Read contents of file with Beautiful Soup, and convert to string
-    - Replace Evernote note link URIs, but not other URIs, with Bear note links
-    - Remove H1 tags from note body
-    - Write to a new file in the bear subdirectory
-    """
-    print(f"Converting {file.name}...")
-    with open(file, "r") as enex:
-        soup = BeautifulSoup(enex, "html.parser")
-        for note in soup.find_all("note"):
-            print(f"* Note {note.title.string}")
-            try:
-                sub_soup = BeautifulSoup(note.content.string, "html.parser")
-                for link in sub_soup.find_all("a", href=re.compile("evernote://")):
-                    link.string = re.sub(BEAR_LINK_ESCAPE, r"\\\1", link.string)
-                    link.insert_before("[[")
-                    link.insert_after("]]")
-                    print(f"** Link {link.string}")
-                    link.unwrap()
-                # for heading in soup.find_all('h1'):
-                # heading.name = 'strong'
-                note.content.string = CData(str(sub_soup))
-            except Exception as e:
-                print(f"!!!!  NO SOUP FOR YOU !!!\n\n{e}\n\n")
-                print(note.content)
+    print(f"Input Directory:  {args.enex_dir}")
+    print(f"Output Directory: {args.output_dir}\n")
 
-        with open(f"{os.path.dirname(file)}/bear/{file.name}", "w") as new_enex:
-            new_enex.write(str(soup))
+    # Create a list of .enex files in the enex_dir
+    fileList = [
+        file
+        for file in os.scandir(args.enex_dir)
+        if file.is_file() and os.path.splitext(file.path)[-1] == ".enex"
+    ]
+
+    # print([ f.name for f in fileList ])
+
+    for file in fileList:
+        print(f"({fileList.index(file)+1:3d}/{len(fileList):3d})  {file.name}")
+        try:
+            with open(os.path.join(args.output_dir, file.name), "x") as f:
+                f.write(parse_enex(file))
+        except Exception as exc:
+            print(f" !!! An error occured: {exc}")
+
+    print("Done.")
 
 
 if __name__ == "__main__":
-    input_enex_path()
+    main()
